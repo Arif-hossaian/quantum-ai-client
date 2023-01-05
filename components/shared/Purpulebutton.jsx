7cf4b77a83adbc61933446acdddd3c464028aafd
Arif-hossaian <arifhossain42136@gmail.com>
2023-01-06 00:25:18 +0600

    change

--- components/shared/Purpulebutton.jsx
+++ components/shared/Purpulebutton.jsx
@@ -1,17 +1,15 @@
 import Link from 'next/link';
 const Button = ({ title }) => {
   return (
-    <Link href="/register" passHref>
-      <button className="z-2 ">
-        <div className="bg-[url('/hero/purpule_button.svg')] bg-no-repeat bg-center bg-contain z-10">
-          <div className=" place-content-center flex justify-center">
-            <div className="h-[90px] md:h-[110px] 2xl:h-[200px] mx-[40px] md:mx-[40px] 2xl:mx-[60px] sm:font-medium text-qiskit-white text-[17px]  2xl:text-[24px] pt-[30px] md:pt-[40px] 2xl:pt-[80px] sm:pt-[30px]">
-              {title}
-            </div>
+    <button className="z-2 ">
+      <div className="bg-[url('/hero/purpule_button.svg')] bg-no-repeat bg-center bg-contain z-10">
+        <div className=" place-content-center flex justify-center">
+          <div className="h-[90px] md:h-[110px] 2xl:h-[200px] mx-[40px] md:mx-[40px] 2xl:mx-[60px] sm:font-medium text-qiskit-white text-[17px]  2xl:text-[24px] pt-[30px] md:pt-[40px] 2xl:pt-[80px] sm:pt-[30px]">
+            {title}
           </div>
         </div>
-      </button>
-    </Link>
+      </div>
+    </button>
   );
 };
 
